7399c0cfddc80014caf92cefe896a8c232ffd8bb
tatjam <tatjam02@gmail.com>
2025-01-26 11:53:16 +0100

    Initial vortices work

--- visualizations/vortex-dynamics/base.js
+++ visualizations/vortex-dynamics/base.js
@@ -18,18 +18,13 @@ function vnorm(v) {
 	return Math.sqrt(v[0] * v[0] + v[1] * v[1]);
 }
 
-// Unitary vortex intensity
-function indvelat(x, vp) {
-	p = vperp(vsub(x, vp));
-	return vscaleby(p, -1.0 / Math.PI / vnorm(p));
-}
 
 // zoomfactor refers to the factor that converts from simulation coordinates to 
 // zero-centered screen coordinates. czoom represents how many "simulation" units
 // are seen by the camera in its largest dimension
 function zoomfactor(camera) {
 	// We use the largest dimension for isotropic scale
-	largedim = Math.max(camera.imgsize[0], camera.imgsize[1]);
+	let largedim = Math.max(camera.imgsize[0], camera.imgsize[1]);
 
 	return largedim / camera.zoom;
 }
@@ -44,9 +39,9 @@ function defaultcamera(imgsize) {
 
 // Returns [[minx,miny], [maxx,maxy]] in simulation coordinates
 function camerabounds(camera) {
-	zf = zoomfactor(camera);
-	min = vsub(camera.center, vscaleby(camera.imgsize, 1.0 / zf));
-	max = vadd(camera.center, vscaleby(camera.imgsize, 1.0 / zf));
+	let zf = zoomfactor(camera);
+	let min = vsub(camera.center, vscaleby(camera.imgsize, 1.0 / zf));
+	let max = vadd(camera.center, vscaleby(camera.imgsize, 1.0 / zf));
 	return [min, max];
 }
 
@@ -54,26 +49,26 @@ function camerabounds(camera) {
 // ar arbitrary. After substracting the camera center (ccenter), they are around (0, 0),
 // and zoom convers them from -imgsize/2 to imgsize/2
 function v2screen(v, camera) {
-	zf = zoomfactor(camera);
-	vscreen = vscaleby(vsub(v, camera.center), zf);
+	let zf = zoomfactor(camera);
+	let vscreen = vscaleby(vsub(v, camera.center), zf);
 	return vadd(vscreen, vscaleby(camera.imgsize, 0.5));
 }
 
 // Draws zero-centered euclidean axes and grid, we draw a relatively solid grid 
 // at integer multiples, and thinner grid at quarter points
 function axes(sk, scale, camera, mainWeight = 2, tickWeight = 1, subtickWeight = 0.1) {
-	mult = 5;
-	sc = scale / mult;
+	let mult = 5;
+	let sc = scale / mult;
 	// Euclidean axes flow at every integer multiple of scale / 4
 	// We draw some lines outside of view but we don't care
-	bounds = camerabounds(camera);
+	let bounds = camerabounds(camera);
 
-	minkx = Math.floor(bounds[0][0] / sc);
-	maxkx = Math.ceil(bounds[1][0] / sc);
-	minky = Math.floor(bounds[0][1] / sc);
-	maxky = Math.ceil(bounds[1][1] / sc);
+	let minkx = Math.floor(bounds[0][0] / sc);
+	let maxkx = Math.ceil(bounds[1][0] / sc);
+	let minky = Math.floor(bounds[0][1] / sc);
+	let maxky = Math.ceil(bounds[1][1] / sc);
 
-	for (kx = minkx; kx < maxkx; kx++) {
+	for (let kx = minkx; kx < maxkx; kx++) {
 		xsc = v2screen([kx * sc, 0], camera)[0];
 		if (kx == 0) {
 			sk.strokeWeight(mainWeight);
@@ -84,7 +79,7 @@ function axes(sk, scale, camera, mainWeight = 2, tickWeight = 1, subtickWeight =
 		}
 		sk.line(xsc, 0, xsc, camera.imgsize[1]);
 	}
-	for (ky = minky; ky < maxky; ky++) {
+	for (let ky = minky; ky < maxky; ky++) {
 		ysc = v2screen([0, ky * sc], camera)[1];
 		if (ky == 0) {
 			sk.strokeWeight(mainWeight);
@@ -94,5 +89,68 @@ function axes(sk, scale, camera, mainWeight = 2, tickWeight = 1, subtickWeight =
 			sk.strokeWeight(subtickWeight);
 		}
 		sk.line(0, ysc, camera.imgsize[0], ysc);
+	}
+}
+
+// We integrate dx/dt = f(x, t) for each vortex
+// f will get called with x and partial dt used (0, 1/2, 1)
+// (any time information must be obtained by f by other means)
+// f must return a 2-vector
+function rk4(x, f, dt, metadata) {
+	let k1 = vscaleby(f(x, 0, metadata), dt);
+	let k2 = vscaleby(f(vadd(x, vscaleby(k1, 0.5)), dt * 0.5, metadata), dt);
+	let k3 = vscaleby(f(vadd(x, vscaleby(k2, 0.5)), dt * 0.5, metadata), dt);
+	let k4 = vscaleby(f(vadd(x, k3), dt, metadata), dt);
+
+	return vadd(x,
+		vadd(
+			vscaleby(k1, 1.0 / 6.0),
+			vadd(
+				vscaleby(k2, 1.0 / 3.0),
+				vadd(
+					vscaleby(k3, 1.0 / 3.0),
+					vscaleby(k4, 1.0 / 6.0)
+				)
+			)));
+}
+
+function makevortex(p, Gamma) {
+	return { p: p, Gamma: Gamma };
+}
+
+// Unitary vortex intensity
+function indvelat(x, vortex) {
+	let p = vperp(vsub(x, vortex.p));
+	return vscaleby(p, -vortex.Gamma / Math.PI / vnorm(p));
+}
+
+// f returns the velocity induced by all vortices at given point
+function simulationf(vortices) {
+	return function (x, dt, effecti) {
+		let totalv = [0, 0];
+		for (let causei = 0; causei < vortices.length; causei++) {
+			if (causei != effecti) {
+				let nvel = indvelat(x, vortices[causei]);
+				totalv = vadd(totalv, nvel);
+			}
+		}
+		return totalv
+	}
+}
+
+function simulate(vortices, dt) {
+	let nextvortp = [];;
+	let f = simulationf(vortices);
+
+	// Compute new positions
+	for (let effecti = 0; effecti < vortices.length; effecti++) {
+		let p = vortices[effecti].p;
+		let nextp = rk4(p, f, dt, effecti);
+		nextvortp.push(nextp);
+	}
+
+	// Assign new positions
+	for (let effecti = 0; effecti < vortices.length; effecti++) {
+		vortices[effecti].p = nextvortp[effecti];
 	}
 }
--- visualizations/vortex-dynamics/viz1.js
+++ visualizations/vortex-dynamics/viz1.js
@@ -2,7 +2,14 @@ const s = (s) => {
 
 	imgsize = [200, 200];
 	camera = defaultcamera(imgsize);
-	camera.zoom = 3;
+	camera.zoom = 10;
+
+	vortices = [
+		makevortex([-1, 1], -1),
+		makevortex([1, 1], -1),
+		makevortex([-1, -1], 1),
+		makevortex([1, -1], 1),
+	]
 
 	s.setup = () => {
 		s.createCanvas(imgsize[0], imgsize[1]);
@@ -11,8 +18,20 @@ const s = (s) => {
 	s.draw = () => {
 		s.background(0xde);
 
+
+		simulate(vortices, 0.1);
+
+
 		s.stroke(0x8e);
 		axes(s, 1.0, camera);
+
+		s.stroke('red')
+		s.strokeWeight(5);
+		for (let vortexi = 0; vortexi < vortices.length; vortexi++) {
+			let vortex = vortices[vortexi];
+			let pos = v2screen(vortex.p, camera);
+			s.point(pos[0], pos[1]);
+		}
 	};
 
 };
